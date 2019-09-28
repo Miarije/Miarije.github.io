c2de427dfca6673d26e474009a6704977e8bf83b
miarije <miarije@live.nl>
2019-09-28 16:52:55 +0200

    Vue module, firebase hosting

---
+++ erik/src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Dit is about vue</h1>
+  </div>
+</template>
---
+++ erik/src/views/Test.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>Button voor push notification</h1>
+    <!--button-->
+  </div>
+</template>
